a2f0575fa945cc8b8ac610878c4a5538de755ed9
Jean Kelvin <jean_kelvin_05@hotmail.com>
2020-03-15 13:56:51 -0300

    In progress

---
+++ src/main/java/com/github/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,26 @@
+package com.github.grpc.greeting.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        System.out.println("Creating stub");
+        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        // do something
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+}
---
+++ src/main/java/com/github/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,26 @@
+package com.github.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        server.awaitTermination();
+    }
+}
